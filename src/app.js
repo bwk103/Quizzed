752ca7ed4d9a92ea3977f24f0f71e49f67acb114
Ben Kielty <bwk103@gmail.com>
2018-03-28 14:31:29 +0100

    Add Mongoose to Dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/quizApp');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
