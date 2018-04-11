5c906a71c3b8465fcaf6eadf8583117a233da55e
Ben Kielty <bwk103@gmail.com>
2018-04-11 13:57:03 +0100

    Changed DB settings

--- src/db/config.js
+++ src/db/config.js
@@ -3,7 +3,7 @@ const config = require('../config');
 
 // const { db: { host, port, name } } = config;
 
-const connectionString = `mongodb://${host}:${port}/${name}`;
+// const connectionString = `mongodb://${host}:${port}/${name}`;
 
 const db = mongoose.connect(process.env.MONGODB_URI || connectionString, {}).then(
   () => {
