74854776c71db0c113e5938eb146fc52aeafe5cd
Ben Kielty <bwk103@gmail.com>
2018-04-11 13:55:42 +0100

    Changed db route

--- src/db/config.js
+++ src/db/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('../config');
 
-const { db: { host, port, name } } = config;
+// const { db: { host, port, name } } = config;
 
 const connectionString = `mongodb://${host}:${port}/${name}`;
 
